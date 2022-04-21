ce1f0613a543cfc087a3a5a036a6b2d6a1c1f98d
Marcelo Henrique <77021035+marcelohenrique95@users.noreply.github.com>
2022-04-21 17:00:21 -0400

    criando constants para modulos

---
+++ src/app/module/cliente/cliente.constants.ts
@@ -0,0 +1,28 @@
+export abstract class ClienteConstants {
+    
+  // LISTA
+  public static readonly TITLE_LIST_CLIENTES = 'Lista de Clientes';
+
+  public static readonly HEADER_TITLE_LISTA_CLIENTE = [
+    'Nome',
+    'Telefone',
+    'Status',
+  ];
+
+  public static readonly MSG_LIST_CLIENTES_VAZIA =
+    'Não há clientes cadastrados.';
+
+  // FORMULARIO
+
+  public static readonly TITLE_FORM_CLIENTE = 'Cadastro de Clientes';
+
+  public static readonly MSG_SUCESSO_CAD_CLIENTE =
+    'Cliente cadastrado com Sucesso !';
+
+  public static readonly MSG_ERRO_CAD_CLIENTE =
+    'Ocorreu um erro ao tentar cadastrar, tente novamente em instantes.';
+
+  // DETAIL
+
+  public static readonly TITLE_DETAIL_CLIENTE = 'Detalhe do Cliente';
+}
---
+++ src/app/module/reserva/reserva.constants.ts
@@ -0,0 +1,32 @@
+export abstract class ReservaConstants {
+
+  // LISTA
+  public static readonly TITLE_LISTA_RESERVA = 
+    'Lista de Reservas';
+
+  public static readonly MSG_LISTA_RESERVA_VAZIA =
+    'Não existem reservas cadastradas.';
+
+  public static readonly HEADER_TITLE_LISTA_RESERVA = [
+    'Cliente',
+    'Data Entrada',
+    'Data Saída',
+    'Situação',
+    'Valor',
+  ];
+
+  // FORM
+  public static readonly TITLE_FORM_RESERVA = 'Cadastro de Reserva';
+
+  public static readonly MSG_SUCESSO_FORM_RESERVA =
+    'Sua reserva foi cadastrada com Sucesso !';
+
+  public static readonly MSG_FAIL_FORM_RESERVA =
+    'Ocorreu um erro ao tentar cadastrar a reserva, tente novamente.';
+
+  public static readonly MSG_CAMPOS_RESERVA =
+    'Os campos do formulário foram limpos.';
+
+  // DETAIL
+  public static readonly TITLE_DETAIL_RESERVA = 'Detalhe de Reserva';
+}
